fb4f676c6eb0a711d9149f087c33150377c4c962
Casper V. Kristensen <casper@vkristensen.dk>
2024-11-25 15:32:53 +0100

    fix: proper sessionmaker middleware
    Avoids `The garbage collector is trying to clean up non-checked-in
    connection <AdaptedConnection <psycopg.AsyncConnection [INTRANS]
    (host=db user=root database=digstsgql) at 0x7f4e41017110>>, which will
    be dropped, as it cannot be safely terminated.  Please ensure that
    SQLAlchemy pooled connections are returned to the pool explicitly,
    either by calling ``close()`` or by using appropriate context managers
    to manage their lifecycle.`

--- digstsgql/app.py
+++ digstsgql/app.py
@@ -1,8 +1,14 @@
 from functools import partial
 
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
 from starlette.requests import Request
 from starlette.responses import Response
+from starlette.types import ASGIApp
+from starlette.types import Receive
+from starlette.types import Scope
+from starlette.types import Send
 from starlette.websockets import WebSocket
 from strawberry.asgi import GraphQL
 from strawberry.dataloader import DataLoader
@@ -24,26 +30,46 @@ class CustomGraphQL(GraphQL):
           - SQLAlchemy database session.
           - Strawberry dataloaders.
         """
-        async with request.app.state.Session() as session:
-            return {
-                "request": request,
-                "response": response,
-                # A single database session ensures a consistent view across
-                # the GraphQL operation.
-                "session": session,
-                # Dataloaders cache results throughout their lifetime, so even
-                # though they should be shared between multiple resolvers, they
-                # need to be instantiated per request.
-                # https://strawberry.rocks/docs/guides/dataloaders#usage-with-context
-                "dataloaders": {
-                    "authors": DataLoader(
-                        load_fn=partial(dataloaders.load_authors, session)
-                    ),
-                    "books": DataLoader(
-                        load_fn=partial(dataloaders.load_books, session)
-                    ),
-                },
-            }
+        session = request.scope["session"]
+        return {
+            "request": request,
+            "response": response,
+            # A single database session ensures a consistent view across
+            # the GraphQL operation.
+            "session": session,
+            # Dataloaders cache results throughout their lifetime, so even
+            # though they should be shared between multiple resolvers, they
+            # need to be instantiated per request.
+            # https://strawberry.rocks/docs/guides/dataloaders#usage-with-context
+            "dataloaders": {
+                "authors": DataLoader(
+                    load_fn=partial(dataloaders.load_authors, session),
+                ),
+                "books": DataLoader(
+                    load_fn=partial(dataloaders.load_books, session),
+                ),
+            },
+        }
+
+
+class SessionMiddleware:
+    """
+    A single database session ensures a consistent view across the GraphQL operation.
+
+    https://www.starlette.io/middleware/#pure-asgi-middleware.
+    """
+
+    def __init__(self, app: ASGIApp, sessionmaker: async_sessionmaker) -> None:
+        self.app = app
+        self.sessionmaker = sessionmaker
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        # Start the single database session for this request, and pass it to
+        # Strawberry through the scope dict.
+        # https://www.starlette.io/middleware/#passing-information-to-endpoints
+        async with self.sessionmaker() as session:
+            scope["session"] = session
+            await self.app(scope, receive, send)
 
 
 def create_app():
@@ -53,12 +79,14 @@ def create_app():
     """
     db.run_upgrade(database_metadata=db.Base.metadata)
 
-    app = Starlette()
-
-    # https://www.starlette.io/applications/#storing-state-on-the-app-instance
     settings = Settings()
-    app.state.settings = settings
-    app.state.Session = db.create_async_sessionmaker(settings.database.url)
+    sessionmaker = db.create_async_sessionmaker(settings.database.url)
+
+    app = Starlette(
+        middleware=[
+            Middleware(SessionMiddleware, sessionmaker=sessionmaker),
+        ],
+    )
 
     graphql_app = CustomGraphQL(schema)
     app.add_route("/graphql", graphql_app)
