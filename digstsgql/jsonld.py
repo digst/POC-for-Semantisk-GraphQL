685859c2ba23a9546643d77b3df41d3007d26c0a
Casper V. Kristensen <casper@vkristensen.dk>
2024-12-11 14:32:25 +0100

    fix: don't set `@id` on GraphQL's `data` key
    We don't know what its id should be, and it's perfectly legal to omit
    it.

--- digstsgql/jsonld.py
+++ digstsgql/jsonld.py
@@ -48,8 +48,14 @@ class JSONLD:
     def as_dict(self) -> dict[str, str]:
         """Convert to JSON-LD @context dict.
 
+        An expanded term definition MUST be a map composed of zero or more keys
+        from @id, @reverse, @type, @language, @container, @context, @prefix,
+        @propagate, or @protected. An expanded term definition SHOULD NOT
+        contain any other keys.
+
         # https://www.w3.org/TR/json-ld/#context-definitions
         # https://www.w3.org/TR/json-ld/#keywords
+        # https://www.w3.org/TR/json-ld11/#expanded-term-definition
         """
         res = {
             "@id": self.id,
@@ -109,11 +115,9 @@ class JSONLDExtension(SchemaExtension):
         """Called for the execution step of the GraphQL query."""
         # Instantiate empty JSON-LD context on every GraphQL execution. A
         # GraphQL response is always nested under the `data` key.
-        context = {
+        context: dict = {
             "@context": {
-                "data": {
-                    "@id": "https://example.org/#TODO",
-                },
+                "data": {},
             },
         }
         self.real_execution_context.context["jsonld_context"] = context
